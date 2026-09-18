3bc9ca007554eb527e0b1f876a6c1a43f1a2f389
Tomás Fossatti <tomas.fossatti01@gmail.com>
2026-09-18 10:01:08 -0300

    fix: type direct pdf-parse parser import

--- src/types/pdf-parse.d.ts
+++ src/types/pdf-parse.d.ts
@@ -1,3 +1,3 @@
-declare module "pdf-parse" {
+declare module "pdf-parse/lib/pdf-parse.js" {
   export default function pdf(data: Buffer): Promise<{ text: string; numpages?: number; info?: unknown }>;
 }
